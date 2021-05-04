8fd654199c6d03312e8283b6f5fea4c5572bcd0c
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-05-04 16:52:56 -0500

    added components folder and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+        <Navbar />
         <h1>Tracker85</h1>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+let Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Tracker85
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
